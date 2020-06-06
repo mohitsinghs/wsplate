d901212e2b1fbbb9b5ea1147a51681425049242e
Mohit Singh <mohit@mohitsingh.in>
2020-06-06 20:55:39 +0530

    Remove apis

--- main.go
+++ main.go
@@ -19,16 +19,6 @@ func main() {
 	hub := newHub()
 	go hub.run()
 
-	// a dummy get endpoint that says hello
-	app.Get("/hello/:name?", func(c *fiber.Ctx) {
-		name := c.Params("name")
-		if name != "" {
-			c.Status(fiber.StatusOK).Send("Hello " + name + " !!")
-		} else {
-			c.Status(fiber.StatusOK).Send("Hello World !!")
-		}
-	})
-
 	// websocket connection handler
 	app.Get("/ws", websocket.New(func(c *websocket.Conn) {
 		// create new client with current connection
